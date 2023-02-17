7e52f7d73fa8de9b1a4e800d92547c6d7afbc316
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-17 14:07:34 -0800

    all possible different combinations of two two-digits.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- *main - print different combinations of two two-digits (00 01, 00 02, ...)
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tens, ones, t, o;
+	int tens;
+	int ones;
+	int t;
+	int o;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		for (ones = '0'; ones <= '8'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (t = tens; t <= '9'; t++)
+		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				for (o = ones; o <= '9'; o++)
+				for (o = ones + 1; o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
@@ -23,18 +26,17 @@ int main(void)
 					putchar(t);
 					putchar(o);
 
-					if (tens == '9' && ones == '8' && t == '9' && o == '9')
+					if (!((tens == '9' && ones == '8') &&
+					      (t == '9' && o == '9')))
 					{
-						break;
+						putchar(',');
+						putchar(' ');
 					}
-					putchar(',');
-					putchar(' ');
 				}
 				o = '0';
 			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
