70928afd2dd0af04a6a733109816bced911d5ec5
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-17 13:23:02 -0800

    all possible different combinations of two two-digits.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = '0'; ones <= '8'; ones++)
 		{
 			for (t = tens; t <= '9'; t++)
 			{
-				for (o = ones + 1; o <= '9'; o++)
+				for (o = ones; o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
@@ -30,6 +30,7 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
+				o = '0';
 			}
 		}
 	}
