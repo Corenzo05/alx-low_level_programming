4a702c5e8823900664674517770d3c67965f50e1
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-22 14:33:01 -0800

    sum of all the multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,6 +2,7 @@
 
 /**
  * main - computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: 0
  */
 
 int main(void)
@@ -20,6 +21,6 @@ int main(void)
 		}
 		c++;
 	}
-	printf("sum of all the multiples of 3 or 5 is: %d\n", multi);
+	printf("%d\n", multi);
 	return (0);
 }
