418ed07677b009fbb65844b5a5f6aed0b463fdeb
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-22 15:39:04 -0800

    prints the sum of the even-valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fionacci sequence up to 4 000 000
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int a, b, sum, sum_ev
+
+	a = 1;
+	b = 1;
+	sum = 1;
+	sum_ev = 0;
+
+	while (b < 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if ((sum <= 4000000) && (sum % 2 == 0))
+			sum_ev += sum;
+	}
+	printf("%d\n", sum_ev);
+
+	return (0);
+}
