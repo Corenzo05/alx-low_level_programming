458653e992f36199511ff84383c063fd4f5f7852
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-23 10:47:27 -0800

    prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,10 +12,10 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for(j = 1; j <= (size - i); j++)
+		for (j = 1; j <= (size - i); j++)
 			_putchar(' ');
 
-		for(k = 1; k <= i; k++)
+		for (k = 1; k <= i; k++)
 			_putchar('#');
 		_putchar('\n');
 	}
