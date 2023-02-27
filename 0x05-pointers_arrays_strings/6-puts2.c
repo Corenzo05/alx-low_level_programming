5e23e2360040c489b3810fda8ad72ca6cd1c55c6
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-27 11:29:30 -0800

    prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,17 @@
 void puts2(char *str)
 {
 	int len = 0;
-
+	
 	while (len >= 0)
 	{
 		if (len % 2 == 0)
 			_putchar(str[len]);
 
-		len++;
 		if (str[len] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
+		len++;
 	}
 }
