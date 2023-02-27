baa0d4a3b34cda6fa5078b8f0849ce2d583a6af8
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-02-27 05:49:18 -0800

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while ((*str + length) != '\0')
+		length++;
+
+	return (length);
+}
