80c38c245bc6dbdfc785501824e67fa26527ba2f
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-02 10:50:35 -0800

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {'a', 'e', 'o', 't', 'l'};
+	int upp_letters[] = {'A', 'E', 'O', 'T', 'L'};
+	int numbers[] = {'4', '3', '0', '7', '1'};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
