ae4155becda73d33503d6149214a7a7ade67822f
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-02 10:28:40 -0800

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * string_toupper - capitalize first letter of all words
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int separators[] = {' ', 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i;
+	int len = 0;
+
+	if (*(s + len) >= 'a' && *(s + len) <= 'z')
+		*(s + len) = *(s + len) - 'a' + 'A';
+	len++;
+	while (*(s + len) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + len) == separators[i])
+			{
+				if ((*(s + (len + 1)) >= 'a') && (*(s + (len + 1)) <= 'z'))
+					*(s + (len + 1)) = *(s + (len + 1)) - ' ';
+				break;
+			}
+		}
+		len++;
+	}
+	return (s)
+}
