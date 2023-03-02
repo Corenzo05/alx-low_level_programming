0628a3fc9729f8ab55cc463b44a2b5f240f9353d
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-02 09:23:34 -0800

    changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,9 +5,9 @@
  * @s: string to manipulate
  * Return: string with all letters capitalized
  */
-
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+
 	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
