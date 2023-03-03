3a77752fa2504898649506eee7a6177f733eac64
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-02 16:18:18 -0800

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int crypte[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int count = 0;
+	int i;
+	
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == letters[i])
+			{
+				*(s + count) == crypte[i];
+				break;
+			}
+		}
+		count++;;
+	}
+	return (s);
+}
