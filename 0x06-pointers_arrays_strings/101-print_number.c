b6997279199a9f7356cf50d2b165b721adb6046c
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-02 16:43:33 -0800

    prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int digit, m, count;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	digit = m;
+	count++;
+
+	while (digit > 9)
+	{
+		digit /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 9; count /= 10)
+		_putchar(((m / count) % 10) + '0');
+}
