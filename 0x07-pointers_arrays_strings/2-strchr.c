a829ddb6077d6dafc1f33714558352f107728428
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-06 06:01:15 -0800

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,14 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0' && s[i] != c) /* find match */
-		i++;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
 
-	if (s[i] == c) /* if match, assign to address */
-		return (&s[i]);
+	if (*(s + i) == c)
+		return (s + i);
 	else
 		return (NULL);
 }
