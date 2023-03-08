4e2f275c4cd0649c284ba04a325eb172eee92053
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-08 11:09:35 -0800

    is a string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * compare - compare head and tail indices for match
+ * @start: index starting from left of string
+ * @end: index starting from right of string, moving backwards
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int compare(char *start, char *end)
+{
+	if (strat >= end)
+		return (1);
+	if (*start == *end)
+		return (compare(strat + 1, end - 1));
+	return (0);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	s++;
+	return (_strlen(s));
+}
+
+/**
+ * is_palindrome - check if palindrome
+ * @s: string to check
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	len = _strlen(s);
+	if (*s == '\0')
+		return (1);
+	return (compare(s, s + (len - 1)));
+}
