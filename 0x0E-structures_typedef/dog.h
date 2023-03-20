c438409e5d3156c3bd619092e521e2cd83955ec4
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-20 08:04:12 -0700

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,8 @@
 #define DOG_H
 
 /**
- * dog_t - typedef for struct dog.
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 /**
@@ -19,5 +18,3 @@ struct dog
 	float age;
 	char *owner;
 };
-
-#endif
