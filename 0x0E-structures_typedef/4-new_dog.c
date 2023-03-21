36cfccb8240a40e00507f721d63c3a04436aefb2
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-20 18:37:59 -0700

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,86 @@
+#include "dog.h"
+
+/**
+ * len - find length of string
+ * @str: string
+ * Return: length
+ */
+
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/*
+ * strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: the pointer to dest
+ */
+
+char *strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i <= len(src))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogy;
+	char *copy_of_name;
+	char *copy_of_owner;
+
+	dogy = malloc(sizeof(dog_t));
+	if (dogy == NULL)
+		return (NULL);
+
+	dogy->age = age;
+
+	if (name != NULL)
+	{
+		copy_of_name = malloc(len(name) + 1);
+		if (copy_of_name == NULL)
+		{
+			free(dogy);
+			return (NULL);
+		}
+		dogy->name = strcpy(copy_of_name, name);
+	}
+	else
+		dogy->name = NULL;
+
+	if (owner != NULL)
+	{
+		copy_of_owner = malloc(len(owner) + 1);
+		if (copy_of_owner == NULL)
+		{
+			free(copy_of_name);
+			free(dogy);
+			return (NULL);
+		}
+		dogy->owner = strcpy(copy_of_owner, owner);
+	}
+	else
+		dogy->owner = NULL;
+
+	return (dogy);
+}
