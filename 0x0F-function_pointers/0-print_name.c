a1211d0d549ee21ee198ecf914b6aa967ff08067
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-22 06:51:24 -0700

    prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,13 @@
-#include 
+#include "function_pointers.h"
+
+/**
+ * print_name - execute a function given as argument
+ * @name: name to print
+ * @f: function to execute
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
