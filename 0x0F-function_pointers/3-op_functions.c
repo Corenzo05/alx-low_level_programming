06379d69e2a9540317d6dac3da9c39f3f0b908ab
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-22 13:34:59 -0700

    updating file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "CALC_H"
+
+/**
+ * op_add - adds two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op-mul - multiplies two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a % b);
+}
