1ab4105bb8021b07b98a069d05e2fc8dc2b5befa
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-22 18:25:07 -0700

    calculator

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,9 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#ifndef CALC_H
+#define CALC_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct op - Struct op
  *
@@ -20,6 +21,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int);
 
 #endif
