07fecffcc1ebf4cec88599a5d964cee41ff949bc
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-03-22 18:23:20 -0700

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
-#include <stdio.h> /* printf */
-#include <stdlib.h> /* atoi */
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
  * main - when user runs main,
@@ -14,33 +13,32 @@
 
 int main(int argc, char *argv[])
 {
-	int n1, n2;
-	int (*f)(int, int);
+	int num1, num2;
+	int (*op)(int, int);
 
-	/* validate input */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	/* convert user input to ints and point to correct operator function */
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-
-	if (f == NULL || (argv[2][1] != '\0'))
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	printf("%d\n", f(n1, n2)); /* calculate via function ptr */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
+	printf("%d\n", op(num1, num2));
 	return (0);
 }
