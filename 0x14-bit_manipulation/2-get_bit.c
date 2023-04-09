b93644895afe8540c34ceeed0572230cd6733e03
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-04-09 10:28:59 -0700

    get bit at given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdion.h>
+
+/**
+ * get_int - get bit at given index
+ * @n: long int
+ * @index: index of bit
+ * Return: the bit at index, or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int max, bit;
+
+	max = sizeof(unsigned long int) * 8;
+	if (index > max)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
