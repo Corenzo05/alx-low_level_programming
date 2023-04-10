8c734de3f8e9031d489c0cf9b93aa14b3ff3f869
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-04-09 22:56:07 -0700

    checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - check the endianness
+ * Return: 0 if big endian, 1if little endian
+ */
+
+int get_endianness(void);
+{
+	char *c;
+	int i = 1;
+
+	c = (char *)&x;
+
+	return ((int) *c);
+}
