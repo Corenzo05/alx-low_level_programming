ce93d5d69f7784e661eb37d127e9613e2eec9867
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-04-09 22:41:36 -0700

    set 0 at given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * clear_bit - set 0 at given index
+ * @n: pointer to an unsigned long int
+ * @index: index of bit to sets
+ * Return: 1 if it worked, -1 if not
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m;
+	unsigned int max;
+
+	m = 0;
+	max = sizeof(unsigned long int) * 8;
+	if (index > max)
+		return (-1);
+
+	m <<= index;
+	*n = m & *n;
+
+	return (1);
+}
