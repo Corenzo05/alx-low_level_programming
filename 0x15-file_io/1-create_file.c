fa051b7b5d3f42a3cd9da93994744eca8510d6bb
Elfahfouhi <soufiane.fahfouhi49@gmail.com>
2023-04-10 17:53:24 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
 		text_content = "";
-		return (1);
-	}
 
 	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_d == -1)
